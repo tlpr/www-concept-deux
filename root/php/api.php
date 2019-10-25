d5ff4149c1ce7e5062c4149fc94d722477c10542
Finn Lis <finn.lis@outlook.com>
2019-10-25 14:39:16 +0200

    Add JSON PUT request handling to SendRequest function, add more functions to control the user accounts, add cool comments

--- root/php/api.php
+++ root/php/api.php
@@ -13,6 +13,9 @@ class ApiFront
 	private const API_ADDRESS = 'http://127.0.0.1/api/v1/';
 	private const API_KEY = '';
 
+	// requires full URL to send request to, request type/method (GET/POST/PUT/DELETE)
+	// and optionally $Headers (in case if not using GET)
+	// returns array with returned data or NULL if operation did not succeeded.
 	private function SendRequest($Http_Address, $Type, $Headers=0)
 	{
 		
@@ -21,7 +24,25 @@ class ApiFront
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $Type);
 		
 		if ( $Headers )
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($Headers));
+		{
+			// Create an JSON object out of the array to
+			// send it through the PUT request.
+			if ( $Type == 'PUT' )
+			{
+				$Headers = json_encode($Headers);
+				
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+				'Content-Type: application/json','Content-Length: '.strlen($Headers)));
+				
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $Headers);
+			}
+			
+			// Otherwise, just send Headers as an array.
+			else
+			{
+				curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($Headers));
+			}
+		}
 			
 		$response = curl_exec($ch);
 		$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -34,18 +55,48 @@ class ApiFront
 		
 	}
 
+	/////////////////////////////////////////
+	// USER MANAGEMENT
 
 	function GetUserInformation ($UserID, $ApiKey=self::API_KEY)
 	{
-		//if (!$ApiKey) $ApiKey = $this->API_KEY;
 
+		$Address = self::API_ADDRESS . "user.php?id=$UserID".
+		"&auth=$ApiKey";
+		
+		return $this->SendRequest($Address, 'GET');
 		
 	}
-
-	function ValidateCredentials($Username, $Password, $ApiKey=self::API_KEY)
+	
+	function CreateNewUser ($Username, $Password, $Email=0, $ApiKey=self::API_KEY)
 	{
 		
-		//if (!$ApiKey) $ApiKey = $this->API_KEY;
+		$Address = self::API_ADDRESS . "user.php?auth=$ApiKey";
+		$Headers = array(
+			'username' => $Username,
+			'password' => $Password,
+		);
+		
+		if ( $Email ) // if not 0
+			$Headers['email'] = $Email;
+		
+		return $this->SendRequest($Address, 'POST', $Headers);
+		
+	}
+	
+	function ModifyUser ($UserID, $SettingsToChange, $ApiKey=self::API_KEY)
+	{
+		
+		$Address = self::API_ADDRESS . "user.php?id=$UserID&auth=$ApiKey";
+		return $this->SendRequest($Address, 'PUT', $SettingsToChange);
+		
+	}
+	
+	/////////////////////////////////////////
+	// CREDENTIALS VALIDATION
+	
+	function ValidateCredentials ($Username, $Password, $ApiKey=self::API_KEY)
+	{
 		
 		$Address = self::API_ADDRESS . "validate.php?act=credentials&username=$Username".
 		"&password=$Password&auth=$ApiKey";
